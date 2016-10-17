77685abedc8c0099fd87ca93321b6c8ed2d3232c
Harrian <the.real.tyler.rau@gmail.com>
2016-10-17 10:28:35 -0500

    Added more heap interaction

--- paraproject.cpp
+++ paraproject.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdlib>
 #include <cstring>
+
 class pixal{
 	private:
 		unsigned char red;
@@ -19,6 +20,7 @@ pixal::pixal(unsigned char a, unsigned char b, unsigned char c){
 	blue=b;
 	green=c;
 }
+
 int main(int argv, char ** argc){
 	//read image
 	FILE * bitmapfile = fopen(argc[1],"r");
@@ -26,35 +28,35 @@ int main(int argv, char ** argc){
 	fread(infotable,sizeof(unsigned char),54,bitmapfile);
 	int imagewidth = *(int*)(infotable+18);
 	int imageheight = *(int*)(infotable+22);
-	std::vector< std::vector<pixal *> * > bigoldarrayofpixals(imageheight,NULL);
+	std::vector< std::vector<pixal *> * > * bigoldarrayofpixals = new std::vector< std::vector<pixal *> * > (imageheight,NULL);
 	for(int i=imageheight-1;i>=0;i--){
-		bigoldarrayofpixals[i]=new std::vector<pixal *>;
+		bigoldarrayofpixals->at(i)=new std::vector<pixal *>;
 		for(int j=0;j<imagewidth;j++){
 			unsigned char * readpixal = new unsigned char[3];
 			fread(readpixal,sizeof(unsigned char),3,bitmapfile);
 //			std::cout<<(int)*(readpixal+2)<<" "<<(int)*(readpixal+1)<<" "<<(int)*(readpixal)<<std::endl;
-			bigoldarrayofpixals[i]->push_back(new pixal(*(readpixal+2),*(readpixal),*(readpixal+1)));
-			delete readpixal;
+			bigoldarrayofpixals->at(i)->push_back(new pixal(*(readpixal+2),*(readpixal),*(readpixal+1)));
+			delete[] readpixal;
 		}
 	}
 	fclose(bitmapfile);
 	FILE * attempttocopy = fopen("attempttocopy.bmp","w");
 	fwrite(infotable,sizeof(unsigned char),54,attempttocopy);
 	unsigned char * img = new unsigned char[3*imageheight*imagewidth];
-	memset(img,0,sizeof(img));
+	memset(img,0,sizeof(unsigned char)*3*imageheight*imagewidth);
 	for(int i=0;i<imageheight;i++){
-		std::vector<pixal *> * magic(bigoldarrayofpixals[imageheight-i-1]);
+		std::vector<pixal *> * magic(bigoldarrayofpixals->at(imageheight-i-1));
 		for(int j=0;j<imagewidth;j++){
 //			std::cout<<(i*imagewidth+j)*3<<" ";
-			img[(i*imagewidth+j)*3]=magic->at(j)->getBlue();
-			// img[(i*imagewidth+j)*3+1]=magic->at(j)->getGreen();
+//			img[(i*imagewidth+j)*3]=magic->at(j)->getBlue();
+			 img[(i*imagewidth+j)*3+1]=magic->at(j)->getGreen();
 			// img[(i*imagewidth+j)*3+2]=magic->at(j)->getRed();
 		}
 //		std::cout<<std::endl;
 	}
 	fwrite(img,sizeof(unsigned char),3*imageheight*imagewidth,attempttocopy);
 	fclose(attempttocopy);
-		
+
 	//std::cout<<bigoldarrayofpixals[0][0]->getRed()<<" "<<bigoldarrayofpixals[0][0]->getBlue()<<" "<<bigoldarrayofpixals[0][0]->getGreen()<<std::endl;
 	return 0;
 }
